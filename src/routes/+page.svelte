5f424eb2a5103b8dfa1e831b9fdb46391cb779d7
snitski <jjsnit@gmail.com>
2025-08-30 15:15:55 -0400

    Add links to resume

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import Fa from 'svelte-fa';
-    import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+    import { faEnvelope, faFileLines } from '@fortawesome/free-regular-svg-icons';
     import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
     import { Button } from "$lib/components/ui/button";
     import * as Card from "$lib/components/ui/card";
@@ -10,23 +10,27 @@
     <h1 class="text-9xl text-primary pb-4">Hey, I'm Alex.</h1>
     <h2 class="text-6xl pb-4">A Software Engineer from New York.</h2>
     <div class="flex flex-wrap gap-2">
-        <Button href="mailto:alex@snit.ski" target="_blank">
+        <Button href="https://resume.snit.ski" variant="outline" class="gap-3" target="_blank">
+            <Fa icon={faFileLines} scale="2" />
+            <p class="font-bold">Resume</p>
+        </Button>
+        <Button href="mailto:alex@snit.ski" variant="outline" class="gap-3" target="_blank">
             <Fa icon={faEnvelope} scale="2" />
             <p class="font-bold">alex@snit.ski</p>
         </Button>
-        <Button href="https://linkedin.com/in/alexander-snit" target="_blank">
+        <Button href="https://linkedin.com/in/alexander-snit" variant="outline" class="gap-3" target="_blank">
             <Fa icon={faLinkedin} scale="2" />
             <p class="font-bold">/in/alexander-snit</p>
         </Button>
-        <Button href="https://github.com/snitski" target="_blank">
+        <Button href="https://github.com/snitski" variant="outline" class="gap-3" target="_blank">
             <Fa icon={faGithub} scale="2" />
             <p class="font-bold">/snitski</p>
         </Button>
     </div>
 </div>
 
-<div class="flex flex-col items-center px-4 md:px-16">
-    <Card.Root id="about-me" class="max-w-4xl">
+<div class="flex flex-col items-center pb-4 px-4 md:px-16 gap-4">
+    <Card.Root id="about-me" class="max-w-4xl w-full">
         <Card.Header>
             <Card.Title>
                 <h2>About Me</h2>
@@ -39,6 +43,17 @@
             by leveraging modern C standards and the latest of DevOps technology.
             In addition to my work, I'm currently pursuing my master's degree in
             computer science at Stony Brook University with a focus in systems and security.
+        </Card.Content>
+    </Card.Root>
+
+    <Card.Root id="resume" class="max-w-4xl w-full">
+        <Card.Header>
+            <Card.Title>
+                <h2>Resume</h2>
+            </Card.Title>
+        </Card.Header>
+        <Card.Content class="text-lg">
+            You can always find my latest resume at <a class="text-primary hover:underline" href="/resume" target="_blank">resume.snit.ski</a>.
         </Card.Content>
     </Card.Root>
 </div>
